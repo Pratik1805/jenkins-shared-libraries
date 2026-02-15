3d0d9e1e37e265c87bcedd79de3b6488010630ae
Pratik Pandey <PRATIKPANDEY0532@GMAIL.COM>
2026-02-15 13:38:37 +0530

    Fix variable name in docker push command

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -6,6 +6,6 @@ def call(String Project, String ImageTag, String dockerhubuser){
                     usernameVariable:"dockerhubuser")]){
        sh "docker login -u ${dockerhubuser} -p ${dockerHubPass}"
      }
-     sh "docker push ${env.dockerhubuser}/${Project}:${ImageTag}"
+     sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
      echo "Docker Image successfully pushed to ${dockerhubuser}"
 }
